c99c6f11f0f097a9e35f72a5c6f11e7ff62b19b2
Cristian Nolè <cristian.nole@icubed.it>
2023-11-20 17:41:48 +0100

    edit connection string

--- Restaurant.API/Program.cs
+++ Restaurant.API/Program.cs
@@ -1,3 +1,5 @@
+using Restaurant.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddSingleton<IDbContextFactory, RestaurantDbContextFactory>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
