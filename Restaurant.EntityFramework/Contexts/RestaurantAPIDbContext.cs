648e8af256b90c4d48c4067bf2bc89ae82658f49
Cristian Nolè <cristian.nole@icubed.it>
2023-11-20 15:47:55 +0100

    add connection string in dbcontext

---
+++ Restaurant.EntityFramework/Contexts/RestaurantAPIDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant.EntityFramework.Contexts
+{
+    public class RestaurantAPIDbContext : DbContext
+    {
+        private string _connectionString { get; set; }
+
+        public RestaurantAPIDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.server(_connectionString);
+
+            base.OnConfiguring(optionsBuilder); 
+        }
+    }
+}
