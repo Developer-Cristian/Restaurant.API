a4513905dd048b694f133784de84f43c51cfd3ee
Cristian Nolè <cristian.nole@icubed.it>
2023-11-20 18:12:33 +0100

    .

---
+++ Restaurant.EntityFramework/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant.Models;
+
+namespace Restaurant.EntityFramework.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private string _connectionString { get; set; }
+
+        public ApplicationDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+
+            base.OnConfiguring(optionsBuilder); 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Dish>()
+                .Property(p => p.Id)
+                .HasDefaultValueSql("NEWID()");
+        }
+
+        /// <summary>
+        /// Dishes db set
+        /// </summary>
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
