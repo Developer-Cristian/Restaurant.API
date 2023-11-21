cf80a79c41d159fa429e56026b35c9c0007aecf1
cristian.nole <cristiannole@hotmail.it>
2023-11-21 18:01:36 +0100

    push con errori

--- Restaurant/Services/SaveServices/ISaveGenericService.cs
+++ Restaurant/Services/SaveServices/ISaveGenericService.cs
@@ -1,11 +1,12 @@
 ﻿using Restaurant.Common.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Restaurant.Services.SaveServices
 {
     public interface ISaveGenericService<T>
         where T : class, IEntity
     {
-        Task<T> SaveAsync(T model);
+        Task<IEnumerable<ValidationResult>> SaveAsync(T model);
 
         Task<T> DelateAsync(Guid? id);
     }
