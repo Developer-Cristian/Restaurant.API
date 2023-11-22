20b3829d9c51a7bf91a609f6b4ec3ce190fbb375
cristian.nole <cristiannole@hotmail.it>
2023-11-22 16:05:34 +0100

    add response

---
+++ Restaurant.API/Controllers/DishesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Contracts.Response;
+
+namespace Restaurant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DishesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Fetch")]
+        [ProducesResponseType(200, Type = typeof(List<DishResponse>)]
+        public async Task<IActionResult> FetchAllDishAsync()
+        {
+        }
+    }
+}
---
+++ Restaurant.Contracts/Response/DrinkResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant.Contracts.Response
+{
+    public class DrinkResponse
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public MenuResponse Menu { get; set; }
+    }
+}
